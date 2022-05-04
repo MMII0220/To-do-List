f4316dd48fff80421bbf69a733cc4be1ab47b163
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2022-05-04 07:36:22 +0300

    cannto delete array element also

--- main.js
+++ main.js
@@ -1,40 +1,61 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const add = document.querySelector('.add_item'),
+  const addForm = document.querySelector('form.fo_rm'),
+    add = addForm.querySelector('.add_item'),
     note = document.querySelector('.input'),
-    erase = document.querySelectorAll('.fa-trash'),
+    erase = document.querySelectorAll('.fas .fa-trash'),
     mark = document.querySelector('.fa-check-square'),
-    text = document.querySelector('.to_do_list'),
-    note__info = document.querySelectorAll('.item');
+    note__info = document.querySelector('.item'),
+    text = document.querySelector('.to_do_list');
 
+  let notes = {
+    note__list: [],
+  };
+
+  addForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    createNotes();
+
+    e.target.reset();
+  });
+
+  // Добавляем Заметки через кнопку
   add.addEventListener('click', () => {
     if (note.value.trim()) {
-      text.innerHTML += `<div class="item"><p>${note.value}</p><div><i class="fas fa-check-square" style="color: limegreen"></i><i class="fas fa-trash" style="color: darkgray"></i></div></div>`;
+      notes.note__list.push(note.value);
     }
-
-    note.value = '';
   });
 
-  note.addEventListener('keydown', (e) => {
-    if (e.target.value.trim()) {
-      if (e.code === 'Enter') {
-        text.innerHTML += `<div class="item">
-        <p>${e.target.value}</p>
-        <div>
-        <i class="fas fa-check-square" style="color: limegreen"></i>
-        <i class="fas fa-trash" style="color: darkgray"></i>
-        </div>
-        </div>`;
+  // Корзина, удаляем Заметки
+  text.addEventListener('click', (event) => {
+    let e = event.target;
+
+    notes.note__list.forEach((item) => {
+      if (e.classList.contains('fa-trash')) {
+        item.textContent = '';
+
+        e.parentNode.parentNode.outerHTML = '';
+
+        console.log(notes.note__list);
       }
-      if (e.code === 'Enter') e.target.value = '';
-    }
-  });
-
-  erase.forEach((item, i) => {
-    item.addEventListener('click', () => {
-      note__info[i].classList.remove('item');
-      note__info[i].textContent = '';
     });
   });
 
-  function asd() {}
+  function createNotes() {
+    // То что изначально будет в экране, все будет стерто. Каждый раз оно будет появляться
+    text.innerHTML = '';
+
+    // Все что будет в обьекте будет появляться.
+    notes.note__list.forEach((item) => {
+      if (item.trim()) {
+        text.innerHTML += `<div class="item">
+          <p class="w">${item}</p>
+          <div>
+            <i class="fas fa-check-square" style="color: limegreen"></i>
+            <i class="fas fa-trash" style="color: darkgray"></i>
+          </div>
+        </div>`;
+      }
+    });
+  }
 });
